327eb1a8bb925a4616c0b1d9bdbea933d960b052
Akash <akashlochan2005@gmail.com>
2025-05-19 12:34:43 +0200

    GameManager prefab and bonsai game done

--- Assets/Devs/Akash/Scripts/BonsaiCutting.cs
+++ Assets/Devs/Akash/Scripts/BonsaiCutting.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class BonsaiCutting : MonoBehaviour
 {
+
+    [SerializeField] private GameObject bonsaiLeaf;
+    [SerializeField] GameObject bonsaiSpawnPoint;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +15,14 @@ public class BonsaiCutting : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("ontrigger");
+        if(other.gameObject.CompareTag("Cup"))
+        {
+            Instantiate(bonsaiLeaf, bonsaiSpawnPoint.transform.position, Quaternion.identity);
+        }
     }
 }
