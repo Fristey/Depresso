735a8d15eef3bd85748880574a88014cf4d33e9a
teshawnwouter <145546485+teshawnwouter@users.noreply.github.com>
2025-06-18 10:38:41 +0200

    fixing UI Text and finish the trash

--- Assets/Devs/Teshawn/Scripts/Trashcan.cs
+++ Assets/Devs/Teshawn/Scripts/Trashcan.cs
@@ -1,13 +1,22 @@
+using Copying;
 using UnityEngine;
 
 public class Trashcan : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Cup"))
+        if (collision.gameObject.GetComponent<MixingCup>() != null)
         {
-            collision.gameObject.GetComponent<MixingCup>().cupIngredientes.Clear();
-            collision.gameObject.GetComponent<MixingCup>().currentAmount = 0;
+            if (collision.gameObject.GetComponent<MixingCup>().drinkToserve == null)
+            {
+                collision.gameObject.GetComponent<MixingCup>().cupIngredientes.Clear();
+                collision.gameObject.GetComponent<MixingCup>().currentAmount = 0;
+            }
+            else
+            {
+                collision.gameObject.GetComponent<MixingCup>().drinkToserve = null;
+                Copy.CopyingComponents(collision.gameObject.GetComponent<MixingCup>().normalCup, collision.gameObject);
+            }
         }
     }
 }
