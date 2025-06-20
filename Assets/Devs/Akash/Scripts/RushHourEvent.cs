dcd786361c3b4b732b27c4c06b83e89e6dd58d30
Akash <akashlochan2005@gmail.com>
2025-06-20 12:02:05 +0200

    RushHour Test Script

--- Assets/Devs/Akash/Scripts/RushHourEvent.cs
+++ Assets/Devs/Akash/Scripts/RushHourEvent.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class RushHourEvent : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float rushHourDuration = 60f; // Duration of the rush hour event in seconds
+    [SerializeField] private int customerIncrease = 5; // Number of additional customers during rush hour
+    private float rushHourTimer;
+    private bool isRushHourActive = false;
+    private void Start()
     {
-        
+        rushHourTimer = rushHourDuration;
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (isRushHourActive)
+        {
+            rushHourTimer -= Time.deltaTime;
+            if (rushHourTimer <= 0)
+            {
+                EndRushHour();
+            }
+        }
+    }
+    public void StartRushHour()
+    {
+        isRushHourActive = true;
+        rushHourTimer = rushHourDuration;
+        CustomerManager.Instance.counterStools.AddRange(new GameObject[customerIncrease]);
+        Debug.Log("Rush Hour started! Increased customer count by " + customerIncrease);
+    }
+    private void EndRushHour()
+    {
+        isRushHourActive = false;
+        CustomerManager.Instance.counterStools.RemoveRange(CustomerManager.Instance.counterStools.Count - customerIncrease, customerIncrease);
+        Debug.Log("Rush Hour ended!");
     }
 }
