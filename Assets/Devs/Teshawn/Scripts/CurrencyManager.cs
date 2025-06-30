4542be60312e85c2aeacdf43bdb3382596781bf7
teshawnwouter <145546485+teshawnwouter@users.noreply.github.com>
2025-06-30 12:11:25 +0200

    currency display

--- Assets/Devs/Teshawn/Scripts/CurrencyManager.cs
+++ Assets/Devs/Teshawn/Scripts/CurrencyManager.cs
@@ -1,11 +1,18 @@
+using TMPro;
 using UnityEngine;
 
 public class CurrencyManager : MonoBehaviour
 {
     public int playerCurrency;
 
+    public TextMeshProUGUI currencyText;
     public void AddCurrency(int amout)
     {
         playerCurrency += amout;
+    }
+
+    private void Update()
+    {
+        currencyText.text = playerCurrency.ToString();
     }
 }
