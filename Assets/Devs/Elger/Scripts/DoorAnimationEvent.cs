2d66a8cd646d997de196791dfa18e0c8b1aec30e
ElgerC <elger.creutzberg@gmail.com>
2025-07-04 22:50:02 +0200

    added door fade

--- Assets/Devs/Elger/Scripts/DoorAnimationEvent.cs
+++ Assets/Devs/Elger/Scripts/DoorAnimationEvent.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 public class DoorAnimationEvent : MonoBehaviour
 {
     Transform camTransform;
+    CamSwapManager swapManager;
     private void Awake()
     {
         camTransform = FindFirstObjectByType<Camera>().transform;
+        swapManager = FindFirstObjectByType<CamSwapManager>();
     }
 
     private void Update()
     {
-        if(Vector3.Distance(camTransform.position,transform.position) < 0.1)
+        if(Vector3.Distance(camTransform.position,transform.position) < 0.1 && swapManager.isLookingAtDoor)
         {
             GameManager.Instance.DoorAnimationFinished();
         }
